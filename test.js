5e83ee5da1908780ba37c1d6fba86c4791b48ea8
trapzobinna <obinnaokeke15@yahoo.com>
2025-07-02 20:03:41 +0100

    update

---
+++ test.js
@@ -0,0 +1,32 @@
+// let a = 300;
+// let b = 500;
+// let sum = a + b;
+// console.log(sum);
+// alert(sum);
+
+
+
+
+
+// let test = document.getElementById("link1");
+// test.textContent = "NOT-HOME";
+// test.style.color = "blue";
+// console.log(test);
+// alert(test);
+
+// let display = document.getElementById("imagesliderr");
+// setTimeout(function(){
+//     img.src = "../day2/bike.jpg";
+// }, 3000);
+
+// function game(){
+//      let message = "game-over";
+//      alert(message);
+//         console.log(message);
+// }
+
+// function myfunction(){
+//     let x = document.getElementsByClassName("hor1h1");
+//     x.style.color = "red";
+//     x.textContent = "METAL CHAIR";
+// }
